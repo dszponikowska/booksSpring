b30dd74e7aa131c992ef1da1207d37548e8b2132
Szymon Miloch <milszym@gmail.com>
2021-05-30 13:26:51 +0200

    Fix location of docs

--- src/main/java/pl/javastart/controller/BookController.java
+++ src/main/java/pl/javastart/controller/BookController.java
@@ -65,10 +65,10 @@ public class BookController {
 
     @DeleteMapping(value = "/books/{id}")
     public BookDto deleteBookById(@PathVariable("id") Long id) {
-        return bookService.deleteBookById(id);
         /**
-         * It delets book and returns book dto.
+         * It deletes book and returns book dto.
          */
+        return bookService.deleteBookById(id);
     }
 
 }
